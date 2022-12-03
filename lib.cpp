d80c4ef7e97f97a4b003aecc78f31c1deef2641b
GiovanniPetruzzelli <117984097+GiovanniPetruzzelli@users.noreply.github.com>
2022-12-03 11:54:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool primi(int n) {
+    int div=1, conta=0
+        while(conta<=1 && div<=n/2) {
+             if(nXdiv==0) {
+                  conta++;
+             }
+          div++;
+        }
+  if (conta==1)
+  {
+    return true:
+  } else{
+    return false;
+  }
+}
